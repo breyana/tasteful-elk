949613cb308b412e57ef590354bd4cf14f9a87c1
Breyana Scales <Breyana@anasauce1.local>
2016-12-14 14:13:11 -0800

    changed name of card images and attempting to change image based on the object key

---
+++ src/card_data.js
@@ -0,0 +1,10 @@
+const cardData = [
+  {cardName: "Two of Cups"
+  cardImage: './CardImages/cups_2.jpg'
+  cardDescription: "To understand the 2 of Cups, all you have to do is look at its image. A man and a woman are gazing at each other, ready to share their cups (emotions). Here is the very picture of romantic and sexual attraction. The energy between these two is almost palpable. The Two of Cups shows the beauty and power that is created when two come together. This is the card that lovers want to see, and, in fact, the Two of Cups is the minor arcana equivalent of the Lovers in many ways."+ <br> +"The Two of Cups has a deeper meaning as well. Whenever two forces are drawn together, there is the potential for bonding. This card can stand for the union of any two entities - people, groups, ideas, or talents. In readings, the Two of Cups tells you to look for connections in your life, especially those that are one-on-one. Now is not the time to separate or stay apart. It is the time to join with another and work as a partnership. If you are in conflict, look for truce and the chance to forgive and be forgiven. If you are struggling with two choices or tendencies within yourself, seek to reconcile them."+ <br>+"Usually, the Two of Cups is welcome in a reading, but it can also sound a note of warning. The energy of Two can be very compelling. If you have ever stood next to two people in love, you know what I'm talking about. They create between themselves a world of their own that can feel exclusionary to outsiders. "Two's company; three's a crowd." Make sure that the tendency to pair off is not creating disharmony in your situation."},
+  {
+
+  },
+  {},
+  {},
+]
