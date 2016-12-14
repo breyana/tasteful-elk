73f593d40edf20ac9e5a22c871956e9d998c2db1
Breyana Scales <Breyana@anasauce1.local>
2016-12-14 14:49:35 -0800

    changed card to have width and changed what positioning function was pointing to

--- src/positioning.js
+++ src/positioning.js
@@ -13,13 +13,13 @@ function fiveCards () {
 
 function changeImage() {
 
-       if (document.getElementById("imgClickAndChange").src == ".CardImages/cardback.jpg")
+       if (document.getElementById("position-1").src == ".CardImages/cardback.jpg")
        {
           var surfacedCard = cardData.map(function(a) {return a.cardImage;});
           document.getElementById('position-1').src = surfacedCard;
        }
        else
        {
-          document.getElementById("imgClickAndChange").src == ".CardImages/cardback.jpg";
+          document.getElementById("position-1").src == ".CardImages/cardback.jpg";
        }
    }
